d274b9cf2712de41682b74c4a70acfaa1245b909
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-07-03 23:49:17 +0545

    ok

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-fs = 500  # sampling frequency
-t = np.arange(0, 1, 1/fs)  # 1 second
-clean_signal = np.sin(2 * np.pi * 50 * t)  # 50 Hz sine wave
-noise = np.random.normal(0, 0.5, len(t))  # white Gaussian noise
-noisy_signal = clean_signal + noise
+num_taps = 51 # it helps to use an odd number of taps
+cut_off = 3000 # Hz
+sample_rate = 32000 # Hz
 
-plt.plot(t, noisy_signal)
-plt.title("Noisy Signal")
+# create our low pass filter
+h = signal.firwin(num_taps, cut_off, fs=sample_rate)
+
+# plot the impulse response
+plt.plot(h, '.-')
 plt.show()
